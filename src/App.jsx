fd468fff61d44eb8f68a3a646d9328b31ccc477c
mortalprotagonist <aktr2003@gmail.com>
2025-04-12 00:09:11 +0530

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css"
 function App() {
 
+  const [count,Setcount]=useState(0)
 
   function handleClick(){
-    alert("Good Choice!!!")
+    alert("Good Choice!!")
+    Setcount(count +1)
   }
   return (
     <div>
       <h1>Hello this is my React website</h1>
       <button onClick={handleClick}>Tap to Learn</button>
+      <p>clicked Button {count} times</p>
     </div>
   )
 }
