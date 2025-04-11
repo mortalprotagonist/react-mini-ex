81d99c1573c03aac43419d7d3356637e015c5c6b
mortalprotagonist <aktr2003@gmail.com>
2025-04-12 00:01:03 +0530

    added function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css"
 function App() {
+
+
+  function handleClick(){
+    alert("Good Choice!!!")
+  }
   return (
     <div>
       <h1>Hello this is my React website</h1>
-      
+      <button onClick={handleClick}>Tap to Learn</button>
     </div>
   )
 }
